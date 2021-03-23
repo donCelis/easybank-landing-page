f67e28ce4bb0f8a4bd024362f67df7c44f8c4f87
Alexander Celis <doncelis@MacBook-Pro-de-Alexander.local>
2021-03-23 07:34:39 -0500

    create style guide

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+//styles
+import "./styles/main.css"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <h1>Multimedia</h1>
   </React.StrictMode>,
   document.getElementById('root')
 );

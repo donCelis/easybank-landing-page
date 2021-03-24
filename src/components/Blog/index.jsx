48f198dd52fb94f7949bdccf34c7e88195883870
Alexander Celis <dacelis0@misena.edu.co>
2021-03-24 16:17:11 -0500

    create card

--- src/components/Blog/index.jsx
+++ src/components/Blog/index.jsx
@@ -5,68 +5,58 @@ import photo2 from "../../images/image-restaurant.jpg";
 import photo3 from "../../images/image-plane.jpg";
 import photo4 from "../../images/image-confetti.jpg";
 
+let data = [
+  {
+    img: photo1,
+    caption: "",
+    title: "Hola Mundo",
+    text: "",
+  },
+  {
+    img: photo2,
+    caption: "",
+    title: "",
+    text: "",
+  },
+  {
+    img: photo3,
+    caption: "",
+    title: "",
+    text: "",
+  },
+  {
+    img: photo4,
+    caption: "",
+    title: "",
+    text: "",
+  },
+];
+
+function Card({ data }) {
+  return (
+    <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-3 mb-4">
+      <article className="card">
+        <img className="card-img-top" src={data.img} alt="" />
+        <div className="card-body">
+          <small>{data.caption}</small>
+          <h5>{data.title}</h5>
+          <p>{data.text}</p>
+        </div>
+      </article>
+    </div>
+  );
+}
+
 export default function Blog() {
   return (
     <section className="blog">
       <div className="container">
         <h2 className="mb-5">Latest Articles</h2>
         <div className="row">
-          <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-3 mb-4">
-            <article className="card">
-              <img className="card-img-top" src={photo1} alt="" />
-              <div className="card-body">
-                <small>By Claire Robinson</small>
-                <h5>Receive money in any currency with no fees</h5>
-                <p>
-                  The world is getting smaller and we’re becoming more mobile.
-                  So why should you be forced to only receive money in a single
-                  …
-                </p>
-              </div>
-            </article>
-          </div>
-          <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-3 mb-4">
-            <article className="card">
-              <img className="card-img-top" src={photo2} alt="" />
-              <div className="card-body">
-                <small>By Claire Robinson</small>
-                <h5>Receive money in any currency with no fees</h5>
-                <p>
-                  The world is getting smaller and we’re becoming more mobile.
-                  So why should you be forced to only receive money in a single
-                  …
-                </p>
-              </div>
-            </article>
-          </div>
-          <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-3 mb-4">
-            <article className="card">
-              <img className="card-img-top" src={photo3} alt="" />
-              <div className="card-body">
-                <small>By Claire Robinson</small>
-                <h5>Receive money in any currency with no fees</h5>
-                <p>
-                  The world is getting smaller and we’re becoming more mobile.
-                  So why should you be forced to only receive money in a single
-                  …
-                </p>
-              </div>
-            </article>
-          </div>
-          <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-3 mb-4">
-            <article className="card">
-              <img className="card-img-top" src={photo4} alt="" />
-              <div className="card-body">
-                <small>By Claire Robinson</small>
-                <h5>Receive money in any currency with no fees</h5>
-                <p>
-                  The world is getting smaller and we’re becoming more mobile.
-                  So why should you be forced to only receive money in a single
-                  …
-                </p>
-              </div>
-            </article>
-          </div>
+          {data.map((element, key) => (
+            <Card data={element} id={key} />
+          ))}
+          {/* <Card {...element} id={key} /> */}
         </div>
       </div>
     </section>
